9263cd14ac877da10fce7552f6a54d2cd8f3c70a
hwangsero <hsro2228@naver.com>
2021-01-05 10:36:58 +0900

    등록

---
+++ src/main/java/socket/practice/ChatController.java
@@ -0,0 +1,36 @@
+package socket.practice;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class ChatController {
+
+    private final RedisPublisher redisPublisher;
+    private final ChatRoomRepository chatRoomRepository;
+
+    /**
+     * websocket "/pub/chat/message"로 들어오는 메시징을 처리한다.
+     */
+    @MessageMapping("/chat/message")
+    public void message(ChatMessage message) {
+        if (ChatMessage.MessageType.ENTER.equals(message.getType())) {
+            chatRoomRepository.enterChatRoom(message.getRoomId());
+            message.setMessage(message.getSender() + "님이 입장하셨습니다.");
+        }
+        // Websocket에 발행된 메시지를 redis로 발행한다(publish)
+        redisPublisher.publish(chatRoomRepository.getTopic(message.getRoomId()), message);
+    }
+}
+
+
+
