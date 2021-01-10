cdfa5eb142a56ae5b694682b9d1aaff1b69ad07a
hwangsero <hsro2228@naver.com>
2021-01-10 12:28:49 +0900

    무중단 배포 테스트

--- src/main/java/socket/PracticeApplication.java
+++ src/main/java/socket/PracticeApplication.java
@@ -2,12 +2,21 @@ package socket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class PracticeApplication {
 
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application.yml,"
+			+ "/app/config/practice/real-application.yml";
+
 	public static void main(String[] args) {
-		SpringApplication.run(PracticeApplication.class, args);
+		new SpringApplicationBuilder(PracticeApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);
 	}
 
 }
